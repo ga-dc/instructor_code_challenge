4ae69ff5fcc7a85a85e115412711a4b45ec878f8
Haleigh Harper <haleighharper@Haleighs-MacBook-Pro.local>
2015-05-20 18:29:53 -0400

    updated express code challenge with mongoose

--- server.js
+++ server.js
@@ -1,33 +1,75 @@
-var express = require('express')
-var app = express()
-var fs = require('fs')
-var path = require('path')
+var express = require('express');
+var app = express();
 var bodyParser = require("body-parser")
+var fs = require('fs');
+var path = require('path');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/codechallenge');
 
-app.use(express.static(path.join(__dirname, '/public')))
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+  console.log("connection started on database:", db.name)
+});
 
-app.use(express.static(path.join(__dirname, 'public')))
+var favoriteSchema = mongoose.Schema({
+    oid: String,
+    name: String
+},{ versionKey: false })
+
+var Favorite = mongoose.model('Favorite', favoriteSchema);
 
 app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data)})
-
-
+  var query = Favorite.find({});
+    query.select("-_id oid name");
+    query.exec(function (err, favorites) {
+      // if (err) return handleError(err);
+      res.setHeader('Content-Type', 'application/json');
+      res.send(favorites);
+    })
+});
+//
+// app.get('/favorites', function(req, res){
+//    console.log("looking for favorites...")
+//    Favorite.find({}), function (err, data)
+//    {
+//         if (err) console.log(err);
+//         else {
+//             console.log(data);
+//         }
+//       }
+//     });
 app.post('/favorites', function(req, res){
-  if(!req.body.name || !req.body.oid){
-    res.send("Error");}
-
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
+Favorite.create(req.body, function(err, favorites) {
+  if (err) return handleError(err);
   res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  res.send(favorites);
 })
+})
+//app.use('/', express.static(path.join(__dirname, 'public')));
+
+// app.get('/favorites', function(req, res){
+//   var data = fs.readFileSync('./data.json');
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
+
+// app.post('/favorites', function(req, res){
+//   if(!req.body.name || !req.body.oid){
+//     res.send("Error");
+//     return
+//   }
+  // var data = JSON.parse(fs.readFileSync('./data.json'));
+  // data.push(req.body);
+  // fs.writeFile('./data.json', JSON.stringify(data));
+//   res.setHeader('Content-Type', 'application/json');
+//   res.send(data);
+// });
 
 app.listen(3000, function(){
   console.log("Listening on port 3000");
-})
+});
