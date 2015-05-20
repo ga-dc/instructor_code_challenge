71d544532ef5a4d388e8220dd678da30fdb447e4
theeltahir <theeltahir@gmail.com>
2015-05-20 19:20:59 -0400

    initial solution

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ var app = express();
 var bodyParser = require("body-parser")
 var fs = require('fs');
 var path = require('path');
+var orm = require('./orm.js');
+orm.connect();
+var Favorite = orm.Favorite;
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,9 +14,15 @@ app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  Favorite.find({}, function(err, favorites){
+    if(err){
+      console.error('Error Saving', err);
+      res.send('Error fetching');
+    }else{
+      res.setHeader('Content-Type', 'application/json');
+      res.send(favorites);
+    }
+  });
 });
 
 app.post('/favorites', function(req, res){
@@ -21,13 +30,17 @@ app.post('/favorites', function(req, res){
     res.send("Error: [oid, name] are required.  Found: '" + Object.keys(req.body) + "'");
     return
   }
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  Favorite.create({oid: req.body.oid, name: req.body.name}, function(err, newFavorite){
+    if(err){
+      console.error('Error Saving', err);
+      res.send('Error saving');
+    }else{
+      res.send(newFavorite);
+    }
+  });
 });
 
-app.listen(3000, function(){
-  console.log("Listening on port 3000");
-});
+  app.listen(3000, function(){
+    console.log("Listening on port 3000");
+  });
+
