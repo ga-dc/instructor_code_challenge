df381c8da551af07bc29bc5d75a1b9bd573702d6
KMG112 <mattwuertz@gmail.com>
2015-05-21 10:27:18 -0400

    git ignore

--- README.md
+++ README.md
@@ -1,11 +1,22 @@
-##Instructor Code Challenge
+##Instructor Code Challenge (extended)
 
 ---
 
 **Back-end Instructions**
 
-- This express applications contains several errors commonly made by students, so you will need to do some debugging to ensure they are working correctly.
-- Fix the errors, and submit a pull request with your solution.
+- Now that you've solved all the problems, let's enhance the app.
+- Replace the file based persistence with MongoDB, using mongoose.js.
+- Continuing on from the solution to last night's homework. Remember:
+
+```
+cd path/to/last_nights_homework
+git checkout starter_with_mongoose
+git checkout -b solution_with_mongoose_YOUR_NAME # i.e. solution_with_mongoose_matt_s
+
+-
+```
+npm install --save mongoose
+```
 
 ## A Working Solution will be able to:
 
@@ -15,3 +26,10 @@
     -`name`
 - `get /favorites`
   - list all favorites created from the above route.
+
+Just like before -- only now it's persisted to MongoDB.
+
+## Bonus:
+
+- /favorites returns the same json as before (no `_id`).
+- Use promises
--- server.js
+++ server.js
@@ -1,33 +1,52 @@
-var express = require('express');
-var app = express();
 var fs = require('fs');
 var path = require('path');
-var bodyParser = require("body-parser")
-
-app.use(express.static(__dirname + '/public'));
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 
-app.get("/favorites", function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+var mongoose = require('mongoose');
+mongoose.connect("mongodb://localhost/milk-n-cookies");
 
-  });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function(callback){
+  console.log("Connection established to to:", db.name)
+})
+
+var movieSchema = mongoose.Schema({
+  name: String,
+  oid: Number
+})
+
+var Movie = mongoose.model("Movie", movieSchema);
+
+ 
+
+// app.use(express.static(path.join(__dirname, '/public')));
+// app.use(bodyParser.urlencoded({ extended: false }))
+// app.use(bodyParser.json());
+
+// app.use('/', express.static(path.join(__dirname, 'public')));
+
+app.get('/favorites', function(req, res){
+  Movie.find(function(err, Movie){
+                  if(err)return console.error(err);
+                  res.send( Movie) ;
+                })
+});
 
 app.post('/favorites', function(req, res){
-
   if(!req.body.name || !req.body.oid){
+    res.send("Error: [oid, name] are required.  Found: '" + Object.keys(req.body) + "'");
     
-    return res.send("Error");
   }
-  
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  var new_movie = new Movie({name: req.body.name, oid: req.body.oid})
+  res.send(new_movie);
+
+  new_movie.save(function(err, movie){
+   if (err) return console.error(err);
+  })
+
+
 });
 
 app.listen(3000, function(){
