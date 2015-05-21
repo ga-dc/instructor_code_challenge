43f5a643ad59d9e7fb28bb47d28da50b3e47f883
Nabil Hashmi <nabil2011@gmail.com>
2015-05-20 21:20:46 -0400

    Complete w10d03 hw

--- server.js
+++ server.js
@@ -1,31 +1,49 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser")
-var fs = require('fs');
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/movies");
+
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function( callback ){
+  console.log("Connection established to ", db.name);
+});
+
 var path = require('path');
 
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
 
+var movieSchema = mongoose.Schema({
+  oid: Number,
+  name: String
+  },
+  {versionKey: false}
+);
+
+var Movie = mongoose.model("Movie", movieSchema);
+
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 app.get('/favorites', function(req, res){
-  var data = fs.readFileSync('./data.json');
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  Movie.find().lean().exec(function(err, movies){
+    res.setHeader('Content-Type', 'application/json');
+    res.send(movies)
+  });
 });
 
 app.post('/favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
     res.send("Error: [oid, name] are required.  Found: '" + Object.keys(req.body) + "'");
-    return
   }
-  var data = JSON.parse(fs.readFileSync('./data.json'));
-  data.push(req.body);
-  fs.writeFile('./data.json', JSON.stringify(data));
-  res.setHeader('Content-Type', 'application/json');
-  res.send(data);
+  var newMovie = Movie.create({
+    oid: req.body.oid,
+    name: req.body.name
+  }, function(err, movie){
+    res.send({oid: movie.oid, name: movie.name});
+  });
 });
 
 app.listen(3000, function(){
