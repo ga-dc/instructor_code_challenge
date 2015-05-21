41bfe5686c530d2b13389081065543745171a78e
luminousbeam <jocelyn.jeriah@gmail.com>
2015-05-21 13:39:34 -0400

    w10d03 hw

--- server.js
+++ server.js
@@ -1,43 +1,36 @@
 var express = require('express');
 var app = express();
 var bodyParser = require("body-parser")
-var fs = require('fs');
+var fs = require('fs')
 var path = require('path');
-var orm = require('./orm.js')
-var Favorite = orm.Favorite;
 
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(bodyParser.urlencoded({ extended: false }))
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/', express.static(path.join(__dirname, 'public')));
 
-app.get('/favorites', function(req, res){
-  var query = Favorite.find({});
-    query.select('-_id oid name'); // interface uses oid, not _id
-    query.exec(function (err, favorites) {
-      if (err) return handleError(err);
-      console.log("Found", favorites.length, "favorites.")
-      res.setHeader('Content-Type', 'application/json');
-      res.send(favorites);
-    })
 
+app.get('/favorites', function(req, res){
+  var data = fs.readFileSync('./data.json')
+  res.setHeader('Content-Type', 'application/json');
+  res.send(data);
 });
+
 
 app.post('/favorites', function(req, res){
   if(!req.body.name || !req.body.oid){
-    res.send("Error: [oid, name] are required.  Found: '" + Object.keys(req.body) + "'");
+    res.send("Error");
     return
   }
-  //Create a favorite
-  Favorite.create(req.body, function (err, favorite) {
-    if (err) return handleError(err);
-    // saved!
-    console.log("We created", favorite.name)
 
-    res.setHeader('Content-Type', 'application/json');
-    res.send(favorite);
-  })
+
+var data = JSON.parse(fs.readFileSync('./data.json')
+data.push(req.body);
+fs.writeFile('./data.json', JSON.stringify(data));
+res.setHeader('Content-Type', 'application/json');
+
+res.send(data);
 });
 
 app.listen(3000, function(){
